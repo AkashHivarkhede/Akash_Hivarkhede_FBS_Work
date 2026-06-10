4f0ca8a9f592d53fd0879dcee5f0506818c4d2eb
Akash_Hivarkhede <139748954+AkashHivarkhede@users.noreply.github.com>
2026-06-10 22:23:11 +0530

    Added first assignment

---
+++ Core_Python/Assignments/Assignments_1/Q3.py
@@ -0,0 +1,10 @@
+# Program to find quotient and remainder of two numbers.¶
+
+Dividend = int(input('Enter Dividend :'))
+Divisor = int(input('Enter Divisor :'))
+
+Quotient = Dividend // Divisor
+Remainder = Dividend % Divisor
+
+print(f'Quotient : {Quotient}')
+print(f'Remainder: {Remainder}')
