ac7c3f5e69f326d4872289e4cd1d6a28cdb17a2b
Akash <sunny.hivarkhede@gmail.com>
2026-09-14 14:09:35 +0530

    all files deleted

--- Assignment_19/Comprehension/Q3.py
+++
@@ -1,7 +0,0 @@
-# Count the number of spaces in a string (take input from user)
-
-string = input("Enter a string: ")
-
-spaces = [i for i in string if i == " "]
-
-print("Number of spaces:", len(spaces))
